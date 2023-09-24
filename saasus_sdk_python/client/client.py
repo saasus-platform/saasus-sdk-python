23277da9fc68534be26b7dc5cb0272d16ec8f162
silent_bayline <silent.bayline@gmail.com>
2023-09-24 22:52:12 +0900

    fix: refresh token

--- saasus_sdk_python/client/client.py
+++ saasus_sdk_python/client/client.py
@@ -1,44 +1,58 @@
 from __future__ import annotations
-
 import hashlib
 import hmac
 import os
 import time
-import logging
-from typing import Any, Callable
+from typing import Any
 from urllib.parse import urlparse, urlencode
-
 from dotenv import load_dotenv
 from saasus_sdk_python import ApiClient
 
 load_dotenv()
 
 
+def serialize_object(obj: Any) -> str | None:
+    if hasattr(obj, 'to_json'):
+        return obj.to_json()
+    else:
+        print("This object does not have to_json method")
+        return None
+
+
 class AuthClient:
-    def __init__(self, time_provider: Callable[[], str] = lambda: time.strftime("%Y%m%d%H%M", time.gmtime())) -> None:
-        self.api_key = os.getenv("SAASUS_API_KEY", "")
-        self.secret_key = os.getenv("SAASUS_SECRET_KEY", "")
-        self.saas_id = os.getenv("SAASUS_SAAS_ID", "")
-        self.base_url = os.getenv("SAASUS_BASE_URL", "https://api.saasus.io/v1")
-        self.time_provider = time_provider
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.referer = None
+            cls._instance.api_key = os.getenv("SAASUS_API_KEY", "")
+            cls._instance.secret_key = os.getenv("SAASUS_SECRET_KEY", "")
+            cls._instance.saas_id = os.getenv("SAASUS_SAAS_ID", "")
+            cls._instance.base_url = os.getenv("SAASUS_BASE_URL", "https://api.saasus.io/v1")
+            cls._instance.time_provider = lambda: time.strftime("%Y%m%d%H%M", time.gmtime())
+        return cls._instance
 
     # 認証用の署名を生成する
-    def generate_signature(self, method: str, url: str, body: str | None, headers: dict | None = None, timestamp: str | None = None) -> dict | None:
-        logging.info(f"Generating signature with method={method}, url={url}, body={body}, headers={headers}, timestamp={timestamp}")
+    def generate_signature(self, method: str, url: str, body: str | None = None, headers: dict | None = None) -> dict:
         literal = "SAASUSSIGV1"
-        # テストを行う際に時間を外部から注入できるようにするためにこのような実装にしているがデフォルトでは現在時刻がUTCで入る
-        timestamp = timestamp if timestamp else self.time_provider()
+        timestamp = self.time_provider()
         parsed_url = urlparse(url)
         host_and_path = parsed_url.netloc + parsed_url.path
         if parsed_url.query:
             host_and_path += "?" + parsed_url.query
-        msg = timestamp + self.api_key + method.upper() + host_and_path + (body if body is not None else "")
+        # POSTなどリクエストボディが空でない場合はJSON文字列に変換する
+        body = serialize_object(body) if body is not None else ""
+        msg = timestamp + self.api_key + method.upper() + host_and_path + body
         signature_hmac = hmac.new(self.secret_key.encode(), msg=msg.encode(), digestmod=hashlib.sha256)
-
-        authorization = "{} Sig={}, SaaSID={}, APIKey={}".format(literal, signature_hmac.hexdigest(), self.saas_id,
-                                                                 self.api_key)
-        headers["Authorization"] = authorization
+        headers = headers or {}
+        headers["Authorization"] = f"{literal} Sig={signature_hmac.hexdigest()}, SaaSID={self.saas_id}, APIKey={self.api_key}"
         return headers
+
+    def set_referer_header(self, header_params: dict) -> dict:
+        if self.referer:
+            header_params["Referer"] = self.referer
+        return header_params
 
 
 class SignedApiClient(ApiClient):
@@ -63,7 +77,7 @@ class SignedApiClient(ApiClient):
 
         # リクエストを行う直前に署名を生成する
         signature_headers = self.auth_client.generate_signature(
-            method, self.configuration.host + resource_path, body, {}
+            method, self.configuration.host + resource_path, body
         )
 
         # header_paramsを署名つきのヘッダで更新する。
@@ -80,4 +94,3 @@ class SignedApiClient(ApiClient):
             collection_formats, _preload_content,
             _request_timeout, _host, _request_auth
         )
-
