28ec25a32e12ef57e6281fce4e962f17d3a6fc9e
yutakahashi114 <yu.takahashi@anti-pattern.co.jp>
2024-03-04 16:56:45 +0900

    modify generate.sh

--- generate.sh
+++ generate.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # 生成するモジュール名の配列
-MODULES=("auth" "pricing" "billing" "awsmarketplace" "integration" "apilog" "communication" )
+MODULES="auth pricing billing awsmarketplace integration apilog communication"
 
 # sdkに含まれる生成したプログラムを削除
 SDK_SRC_DIR="saasus_sdk_python/src"
 SDK_TEST_DIR="test"
 
 # 削除対象ディレクトリ
-DELETE_DIRS=("generated")
+DELETE_DIRS="generated"
 
-for module in "${MODULES[@]}"
+for module in ${MODULES}
 do
     if [ -d ${SDK_SRC_DIR}/${module} ]; then
         rm -rf ${SDK_SRC_DIR}/${module}
@@ -26,7 +26,7 @@ done
 rm -rf ./docs/*
 echo "delete pythondoc success"
 
-for dir in "${DELETE_DIRS[@]}"
+for dir in ${DELETE_DIRS}
 do
     if [ -d ./${dir} ]; then
         rm -rf ./${dir}
@@ -34,7 +34,7 @@ do
 done
 
 #Pythonクラスを生成
-for module in "${MODULES[@]}"
+for module in ${MODULES}
 do
     if [ "${module}" = "integration" ]; then
         target="/local/${module}.yml"
@@ -49,7 +49,7 @@ do
     --package-name saasus_sdk_python.src.${module}
 done
 
-for module in "${MODULES[@]}"
+for module in ${MODULES}
 do
     # プログラム
     cp -pr "saasus_sdk_python/generated/${SDK_SRC_DIR}/${module}" "${SDK_SRC_DIR}/${module}"
