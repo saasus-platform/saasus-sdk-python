e9d399b155946cb2370da5243ad9421cf6fbf4dd
yutakahashi114 <yu.takahashi@anti-pattern.co.jp>
2024-03-21 17:49:59 +0900

    update client

--- saasus_sdk_python/client/apilog_client.py
+++ saasus_sdk_python/client/apilog_client.py
@@ -1,7 +1,8 @@
 import os
 from saasus_sdk_python.client.client import Client
+from urllib.parse import urlencode
 from saasus_sdk_python.src.apilog.api_client import ApiClient
-from saasus_sdk_python.src.apilog import rest
+
 
 class SignedApilogApiClient(ApiClient):
 
@@ -10,21 +11,26 @@ class SignedApilogApiClient(ApiClient):
         self.client = Client()
         self.configuration.default_headers = {}
         self.base_url = os.getenv("SAASUS_BASE_URL", "https://api.saasus.io/v1")
-        self.configuration.host = f"{self.base_url}/apilog"
 
-    def call_api(
-        self,
-        method,
-        url,
-        header_params=None,
-        body=None,
-        post_params=None,
-        _request_timeout=None
-    ) -> rest.RESTResponse:
+    def call_api(self, resource_path, method,
+                 path_params=None, query_params=None, header_params=None,
+                 body=None, post_params=None, files=None,
+                 response_types_map=None, auth_settings=None,
+                 async_req=None, _return_http_data_only=None,
+                 collection_formats=None, _preload_content=True,
+                 _request_timeout=None, _host=None, _request_auth=None):
+
+        if path_params:
+            resource_path = resource_path.format(**path_params)
+        if query_params:
+            query_string = urlencode(query_params)
+            resource_path += f"?{query_string}"
+
+        self.configuration.host = f"{self.base_url}/apilog"
 
         # 署名ヘッダを生成してリクエストヘッダに追加する
         signature_headers = self.client.generate_signature(
-            method, url, body
+            method, self.configuration.host + resource_path, body
         )
 
         # header_paramsを署名つきのヘッダで更新する。
@@ -34,10 +40,11 @@ class SignedApilogApiClient(ApiClient):
 
         # APIクライアントのcall_apiメソッドを呼び出して、署名付きのリクエストを行う
         return super().call_api(
-            method=method,
-            url=url,
-            header_params=header_params,
-            body=body,
-            post_params=post_params,
-            _request_timeout=_request_timeout
+            resource_path, method,
+            path_params, query_params, header_params,
+            body, post_params, files,
+            response_types_map, auth_settings,
+            async_req, _return_http_data_only,
+            collection_formats, _preload_content,
+            _request_timeout, _host, _request_auth
         )
--- saasus_sdk_python/client/auth_client.py
+++ saasus_sdk_python/client/auth_client.py
@@ -1,32 +1,36 @@
 import os
 from saasus_sdk_python.client.client import Client
+from urllib.parse import urlencode
 from saasus_sdk_python.src.auth.api_client import ApiClient
-from saasus_sdk_python.src.auth import rest
+
 
 class SignedAuthApiClient(ApiClient):
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         self.client = Client()
-        self.base_url = os.getenv("SAASUS_BASE_URL", "https://api.dev.saasus.io/v1")
+        self.base_url = os.getenv("SAASUS_BASE_URL", "https://api.saasus.io/v1")
+        super().__init__(*args, **kwargs)
+
+    def call_api(self, resource_path, method,
+                 path_params=None, query_params=None, header_params=None,
+                 body=None, post_params=None, files=None,
+                 response_types_map=None, auth_settings=None,
+                 async_req=None, _return_http_data_only=None,
+                 collection_formats=None, _preload_content=True,
+                 _request_timeout=None, _host=None, _request_auth=None):
+
+        if path_params:
+            resource_path = resource_path.format(**path_params)
+        if query_params:
+            query_string = urlencode(query_params)
+            resource_path += f"?{query_string}"
+
         self.configuration.host = f"{self.base_url}/auth"
-
-    def call_api(
-        self,
-        method,
-        url,
-        header_params=None,
-        body=None,
-        post_params=None,
-        _request_timeout=None
-    ) -> rest.RESTResponse:
-
 
         # 署名ヘッダを生成してリクエストヘッダに追加する
         signature_headers = self.client.generate_signature(
-            method, url, body
+            method, self.configuration.host + resource_path, body
         )
-        print(method, url, body)
 
         # header_paramsを署名つきのヘッダで更新する。
         if header_params is None:
@@ -35,10 +39,11 @@ class SignedAuthApiClient(ApiClient):
 
         # APIクライアントのcall_apiメソッドを呼び出して、署名付きのリクエストを行う
         return super().call_api(
-            method=method,
-            url=url,
-            header_params=header_params,
-            body=body,
-            post_params=post_params,
-            _request_timeout=_request_timeout
+            resource_path, method,
+            path_params, query_params, header_params,
+            body, post_params, files,
+            response_types_map, auth_settings,
+            async_req, _return_http_data_only,
+            collection_formats, _preload_content,
+            _request_timeout, _host, _request_auth
         )
